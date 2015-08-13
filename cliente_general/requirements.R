02e8b341a0954b74b3af3975d5acd44a231d6866
twehrmann <thilo.wehrmann@conabio.gob.mx>
2015-08-13 16:15:52 -0500

    add R dependencies

--- cliente_general/requirements.R
+++ cliente_general/requirements.R
@@ -1,3 +1,19 @@
-install.packages("doBy", "relimp", "logging", "ggplot2", "grid", "gridExtra", "RPostgreSQL", "RUnit", "WriteXLS", "yaml")
+install.packages("doBy", repos='http://cran.us.r-project.org')
+install.packages("relimp", repos='http://cran.us.r-project.org')
+
+install.packages("logging", repos='http://cran.us.r-project.org')
+
+install.packages("ggplot2", repos='http://cran.us.r-project.org')
+install.packages("grid", repos='http://cran.us.r-project.org')
+install.packages("gridExtra", repos='http://cran.us.r-project.org')
+install.packages("RUnit", repos='http://cran.us.r-project.org')
+install.packages("RPostgreSQL", repos='http://cran.us.r-project.org')
+
+install.packages("WriteXLS", repos='http://cran.us.r-project.org')
+install.packages("yaml", repos='http://cran.us.r-project.org')
+
+install.packages("/myapp/installation/Carbono5_1.0.tar.gz", repos = NULL, type="source")
+install.packages("/myapp/installation/dCarbono_1.0.tar.gz", repos = NULL, type="source")
+
 
 #dCarbono, Carbono5
